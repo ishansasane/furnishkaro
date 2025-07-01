76ae906340f71916dd4f9ff6e82971a2cf4a7ace
Sanjyot Kakade <sanjyotkakade9@gmail.com>
2025-07-01 21:17:19 +0530

    addsitepage all done

--- src/assets/pages/AddSitePage.tsx
+++ src/assets/pages/AddSitePage.tsx
@@ -281,7 +281,7 @@ function AddSitePage() {
       <div className="flex justify-end">
         <button
           onClick={handleSaveProjectAndPayment}
-          className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700"
+          className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
         >
           Save Project & Create Payment
         </button>
